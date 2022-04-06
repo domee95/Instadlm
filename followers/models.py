6b20ec6b01f6058952e596885243722573439591
domee95 <domenika_96@hotmail.com>
2022-04-06 13:33:42 +0200

    Creamos los test para las funciones'get_followers' y 'get following'

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Relationship(models.Model):
+
+    origin = models.ForeignKey(User, related_name='relationship_origin', on_delete=models.CASCADE,)
+    target = models.ForeignKey(User, related_name='relationship_target', on_delete=models.CASCADE,)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #def __str__(self):
+        #return self.target
---
+++ followers/utils.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+
+from followers.models import Relationship
+
+
+def get_followers(user):
+    followers = list()
+    for relationship in user.relationship_target.select_related('origin'):
+        followers.append(relationship.origin)
+    return followers
+
+
+def get_following(user):
+    following = list()
+    for relationship in user.relationship_origin.select_related('target'):
+        following.append(relationship.target)
+    return following
